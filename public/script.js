4fcb470c97fa9ac82ebae6276b5344823339c88f
Luminara97 <devikapdinesh4000@gmail.com>
2026-02-28 16:20:22 +0530

    api change2

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ async function login() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch("/api/login", {
+    const response = await fetch("https://tiz-dev.github.io/EcoStreakproject/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -17,38 +17,4 @@ async function login() {
     } else {
         alert(data.message);
     }
-}
-async function loadData() {
-    const userRes = await fetch("/api/user");
-    const user = await userRes.json();
-
-    const taskRes = await fetch("/api/task");
-    const task = await taskRes.json();
-
-    document.getElementById("username").innerText =
-        "Hello, " + user.name + " 👋";
-
-    document.getElementById("streak").innerText =
-        "🔥 " + user.streak + " Day Streak | XP " + user.xp;
-
-    const button = document.getElementById("completeBtn");
-
-    if (task.completed) {
-        button.innerText = "Streak Completed ✅";
-        button.disabled = true;
-    } else {
-        button.innerText = "Mark as Complete";
-        button.disabled = false;
-    }
-}
-async function completeTask() {
-    const res = await fetch("/api/complete", {
-        method: "POST"
-    });
-
-    const data = await res.json();
-
-    alert(data.message);
-
-    loadData();
 }
